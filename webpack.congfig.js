e3b392cb0b70495094f4d4263fd2426ae25765da
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-10 23:25:37 +0000

    adds bable loader syntax in webpack.config.js and others

--- webpack.congfig.js
+++ webpack.congfig.js
@@ -1,26 +1,56 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'build tools',
+            template: "index.html",
+        })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        open: true,
+        host: "localhost",
+      },
     module: {
         rules: [
             {
-                test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+              },
             {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+              },
+              {
                 test: /\.css$/i,
-                use: ['style-loader', "css-loader"],
-                // use: [stylesHandler, "css-loader", "postcss-loader"],
+                use: ["style-loader", "css-loader"],
               },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
               },
         ]
-    }
+    },
+    optimization: {
+        runtimeChunk: 'single',
+      },
 };
